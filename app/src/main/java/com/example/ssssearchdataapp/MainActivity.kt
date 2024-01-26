49b094654a754596f39e71280ca8bb74684aedbb
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-26 11:03:43 +0900

    git init

--- app/src/main/java/com/example/ssssearchdataapp/MainActivity.kt
+++ app/src/main/java/com/example/ssssearchdataapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.ssssearchdataapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.ssssearchdataapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val main = binding.root
+        setContentView(main)
     }
 }
