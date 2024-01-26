c818b59551677dd7cb3f9f8e3c4295513b867775
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-26 15:45:04 +0900

    add : EditText 포커스 획득, 해제 / 외부 화면 터치 시 키보드 숨기기 / 뒤로가기 버튼 클릭 시 종료 다이얼로그 띄우기(MainActivity)

--- app/src/main/java/com/example/ssssearchdataapp/MainActivity.kt
+++ app/src/main/java/com/example/ssssearchdataapp/MainActivity.kt
@@ -1,14 +1,67 @@
 package com.example.ssssearchdataapp
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.example.ssssearchdataapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val main = binding.root
         setContentView(main)
+
+        val fragment = ImageSearchFragment.newInstance("","")
+        setFragment(fragment)
+
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+    }
+
+    fun setFragment(f: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, f)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
+    }
+
+    // EditText 터치 시 포커스 획득 / 외부화면 터치 시 포커스 해제
+    fun getFocused(v: View) {
+        when(v) {
+            binding.etSearchBar -> currentFocus
+            else -> currentFocus?.clearFocus()
+        }
+    }
+
+    // 외부 화면 터치 시 키보드 숨기기(Manifest에 키보드 기본설정 필수(windowSoftInputMode))
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val imm:InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        return super.dispatchTouchEvent(ev)
+    }
+
+    // 뒤로가기 버튼 클릭 시 종료 다이얼로그 띄우기
+    val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val dialog = AlertDialog.Builder(this@MainActivity)
+                .setTitle("종료")
+                .setMessage("종료하시겠습니까?")
+
+            dialog.setPositiveButton("확인") { dialog, _ ->
+                if(!isFinishing) finish()
+            }
+            dialog.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
     }
 }
